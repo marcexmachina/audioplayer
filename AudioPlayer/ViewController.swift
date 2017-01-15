1e118eb2f066e17671233e001862fd068fc3e1bf
Marc <marc.oneill2@gmail.com>
2017-01-15 19:44:33 +1100

    initial commit

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let path = Bundle.main.path(forResource: "parabola", ofType: "mp3")
+    var player = AVAudioPlayer()
+    var timer = Timer()
+    
+    //0 for pause, 1 for play
+    var state = 0
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var scrubber: UISlider!
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        player.setVolume(slider.value, fadeDuration: 0)
+    }
+    
+    @IBAction func scrubberMoved(_ sender: Any) {
+       player.currentTime = TimeInterval(scrubber.value)
+    }
+    
+    @IBAction func playPressed(_ sender: UIButton) {
+        
+        if state == 0 {
+            playPauseButton.setTitle("Pause", for: .normal)
+            player.prepareToPlay()
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+            player.play()
+            state = 1
+        } else if state == 1 {
+            playPauseButton.setTitle("Play", for: .normal)
+            player.pause()
+            state = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            try player =  AVAudioPlayer(contentsOf: URL(fileURLWithPath: path!))
+            player.numberOfLoops = 1
+            
+            scrubber.minimumValue = 0
+            scrubber.maximumValue = Float(player.duration)
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    func updateScrubber() {
+        scrubber.setValue(Float(player.currentTime), animated: false)
     }
 
     override func didReceiveMemoryWarning() {
